63a6261f805026a4c992352f489db99715c5948f
Kan3103 <khang310304@gmail.com>
2023-07-30 12:16:35 +0700

    RWD/GUI - Initial Set up

--- GUI/src/App.js
+++ GUI/src/App.js
@@ -1,23 +1,80 @@
-import logo from './logo.svg';
-import './App.css';
 
+import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+       <h1 >HOME PAGE</h1>
+    <div class="tool">
+        <div class="nav">
+            <img class="logo" alt="hi"src="logo.png"/>
+            <div class="ull">
+                <div class="dropdown">
+                    Event
+                    <div class="dropdown-content">
+                        <a href="https://www.youtube.com/">youtube</a>
+                    </div>
+                </div>
+                <div class="dropdown">Competition
+                    <div class="dropdown-content">
+                        <a href="https://www.youtube.com/">youtube</a>
+                    </div>
+                </div>
+                <div class="dropdown">Program
+                    <div class="dropdown-content">
+                        <a href="https://www.youtube.com/">youtube</a>
+                    </div>
+                </div>
+                <div class="dropdown">About Us
+                    <div class="dropdown-content">
+                        <a href="https://www.youtube.com/">youtube</a>
+                    </div>
+                </div>
+                <div class="dropdown">Project Partners
+                    <div class="dropdown-content">
+                        <a href="https://www.youtube.com/">youtube</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="search">
+            <input class="inputt" type="search" name="" placeholder="Search..."/>
+            <button class="ser">Search</button>
+        </div>
+    </div>
+    <div class="main">
+        <div class="exchange">
+            <div class="pre">
+               <button className='pre_btn' >
+                    pre
+               </button>
+            </div>
+            <div class="next">
+                <button className='next_btn'>
+                    next
+                </button>
+            </div>
+        </div>
+        <div class="gallery">
+        <div class="slide">
+                <img src="background.PNG" alt=""/>
+            </div>
+            <div class="slide">
+                <img src="hinh1.jpg" alt=""/>
+            </div>
+            <div class="slide">
+                <img src="hinh2.jpg" alt=""/>
+            </div>
+            <div class="slide">
+                <img src="hinh3.jpg" alt=""/>
+            </div>
+            <div class="slide">
+                <img src="background.PNG" alt=""/>
+            </div>
+            <div class="slide">
+                <img src="hinh1.jpg" alt=""/>
+            </div>
+        </div>
+    </div>
     </div>
   );
 }
---
+++ GUI/src/run.js
@@ -0,0 +1,54 @@
+import { useEffect } from 'react';
+function Run() {
+  useEffect(() => {
+    const gallery = document.querySelector(".gallery");
+    let currentIndex = 0;
+    let maxIndex = 5; // Số lượng ảnh trong dãy (từ 0 đến 5)
+    let next = document.querySelector(".next_btn");
+    let pre = document.querySelector(".pre_btn");
+    let timer;
+    let slides = document.querySelectorAll(".slide");
+    
+    function slideImage(direction) {
+      currentIndex += direction;
+      var translateValue = -currentIndex * (100 / (maxIndex + 1));
+      gallery.style.transform = `translateX(${translateValue}%)`;
+      gallery.style.transition = "transform 0.5s ease-in-out";
+    }
+
+    gallery.addEventListener("transitionend", () => {
+      if (currentIndex >= maxIndex) {
+        slides = document.querySelectorAll(".slide");
+        currentIndex = 1;
+        gallery.style.transition = "none";
+        var translateValue = -currentIndex * (100 / (maxIndex +1));
+        gallery.style.transform = `translateX(${translateValue}%)`;
+      }
+    });
+
+    function startTimer() {
+      timer = setTimeout(function () {
+        slideImage(1); // Chuyển đến ảnh tiếp theo
+        startTimer(); // Bắt đầu lại đếm thời gian
+      }, 5000); // Đếm thời gian 5 giây
+    }
+
+    function resetTimer() {
+      clearTimeout(timer);
+      startTimer();
+    }
+
+    // Bắt đầu đếm thời gian khi trang web được tải
+    startTimer();
+
+    // Thiết lập sự kiện cho tương tác người dùng (ví dụ: click, hover, ...)
+    next.addEventListener("click",()=> slideImage(1));
+    next.addEventListener("click",()=> resetTimer());
+    pre.addEventListener("click", ()=> slideImage(-1));
+    pre.addEventListener("click", ()=> resetTimer());
+  }, []);
+
+  // Slide end
+  return null;
+}
+export default Run;
