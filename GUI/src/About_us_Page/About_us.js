bce3b619dae3bd6469ce58e72a4cd740f1494138
Kan3103 <khang310304@gmail.com>
2023-08-03 20:59:47 +0700

    RWD/GUI - Add About Us

--- GUI/src/About_us_Page/About_us.js
+++ GUI/src/About_us_Page/About_us.js
@@ -3,6 +3,9 @@ import React from "react";
 function About_us(){
     return(
         <div>
+            <header>
+                <img src="../Images/logo_youth_exchange.png" alt=""></img>
+            </header>
         </div>
     )
 }
