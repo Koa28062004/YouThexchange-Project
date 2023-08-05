2c707bc0f842daea3fdf9ef3a1310644f216da15
Kan3103 <khang310304@gmail.com>
2023-08-05 18:24:25 +0700

    RWD/GUI - Update Homepage

--- GUI/src/Homepage/Home_Information/More_in4.js
+++ GUI/src/Homepage/Home_Information/More_in4.js
@@ -4,11 +4,13 @@ import "./More_in4.css";
 const Contact = [
   {
     mess: "Facebook",
+    class:"F",
     link: "https://www.facebook.com/profile.php?id=100094353529762&mibextid=LQQJ4d",
     idclass: "fa-brands fa-facebook"
   },
   {
     mess: "Instagram",
+    class:"I",
     link: "https://www.instagram.com/youthexchange.project/",
     idclass: "fa-brands fa-instagram"
   }
@@ -17,19 +19,24 @@ const Contact = [
 function More() {
   return (
     <div className="more_inf">
-      <button className="btn_moreinf">How to register</button>
-      <button className="btn_moreinf">Projects</button>
-      <button className="btn_moreinf">
-        Contact
-        <div className="contact">
-          {Contact.map((brand) => (
-            <a href={brand.link} >
-                <i className={brand.idclass} ></i>
-              <span className="mess">{brand.mess}</span>
-            </a>
-          ))}
-        </div>
-      </button>
+        <button className="btn_moreinf">How to register</button>
+        <button className="btn_moreinf">
+            Projects
+            <div className="Project">
+            <i class="fa-regular fa-circle-play"></i> Videos
+            </div>
+        </button>
+        <button className="btn_moreinf">
+            Contact
+            <div className="contact">
+            {Contact.map((brand) => (
+                <a href={brand.link} className={brand.class}>
+                    <i className={brand.idclass} ></i>
+                <span className="mess">{brand.mess}</span>
+                </a>
+            ))}
+            </div>
+        </button>
     </div>
   );
 }
