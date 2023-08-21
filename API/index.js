40a035da5879fd7aec7093336c6c426192fdf1e0
vtphatt2 <thinhphat544@gmail.com>
2023-08-21 17:26:03 +0700

    RWD/API - change type of API returned into objects

--- API/index.js
+++ API/index.js
@@ -9,24 +9,29 @@ const app = express()
 app.use(cors())
 
 app.get('/api/:search', function(req, res) {
-    var search = req.params.search;
+  const search = req.params.search.toLowerCase();
 
-    if (search == "") {
-      res.json([]);
-    }
-    else {
-      fs.readFile('data.txt', 'utf8', (err, data) => {
-        if (err) {
-            console.error('Error reading file:', err);
-            return res.status(500).json({ error: 'Error reading file' });
-        }
+  if (search === "") {
+    res.json([]);
+  }
+  else {
+    fs.readFile('data.txt', 'utf8', (err, data) => {
+      if (err) {
+          console.error('Error reading file:', err);
+          return res.status(500).json({ error: 'Error reading file' });
+      }
 
-        const lines = data.trim().split('\n');
-        const result = lines.filter(line => line.toLowerCase().includes(search.toLowerCase()));
+      const lines = data.trim().split('\n');
+      const result = lines
+        .map(line => {
+          const [key, link] = line.split(' - ').map(item => item.trim().replace(/"/g, ''));
+          return { search: key, link };
+        })
+        .filter(item => item.search.toLowerCase().includes(search));
 
-        res.json(result);
-      });
-    }
+      res.json(result);
+    });
+  }
 });
  
 app.listen(port, () => console.log(`Server is starting at PORT ${port}`));
