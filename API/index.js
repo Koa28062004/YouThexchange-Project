493bcd3201a6bcd5b527a0f1226f23e2e56c5bce
vtphatt2 <thinhphat544@gmail.com>
2023-09-03 19:38:49 +0700

    RWD/API - change data.txt into array named data

--- API/index.js
+++ API/index.js
@@ -1,37 +1,33 @@
 const express = require('express');
 const cors = require('cors');
-const fs = require('fs');
- 
+
 const port = 8000;
- 
-const app = express()
- 
-app.use(cors())
 
-app.get('/api/:search', function(req, res) {
-  const search = req.params.search.toLowerCase();
+const app = express();
 
-  if (search === "") {
-    res.json([]);
-  }
-  else {
-    fs.readFile('data.txt', 'utf8', (err, data) => {
-      if (err) {
-          console.error('Error reading file:', err);
-          return res.status(500).json({ error: 'Error reading file' });
-      }
+app.use(cors());
 
-      const lines = data.trim().split('\n');
-      const result = lines
-        .map(line => {
-          const [key, link] = line.split(' - ').map(item => item.trim().replace(/"/g, ''));
-          return { search: key, link };
-        })
-        .filter(item => item.search.toLowerCase().includes(search));
+const data = [
+    {"search": "Homepage", "link": "/"},
+    {"search": "About us", "link": "/About_us"},
+    {"search": "Core Team", "link": "/About_us/Core_Team"},
+    {"search": "SDGs", "link": "/SDGs"},
+    {"search": "Subtopic 1 (An Overview)", "link": "/Pilot_Program/Project_1"},
+    {"search": "Subtopic 2 (Product outcomes)", "link": "/Pilot_Program/Project_2"},
+    {"search": "Project", "link": "/Project"},
+    {"search": "Project 1", "link": "/Project1"},
+    {"search": "Project 2", "link": "/Project2"}
+];
 
-      res.json(result);
-    });
-  }
+app.get('/api/:search', function (req, res) {
+    const search = req.params.search.toLowerCase();
+
+    if (search === "") {
+        res.json([]);
+    } else {
+        const result = data.filter(item => item.search.toLowerCase().includes(search));
+        res.json(result);
+    }
 });
- 
+
 app.listen(port, () => console.log(`Server is starting at PORT ${port}`));
