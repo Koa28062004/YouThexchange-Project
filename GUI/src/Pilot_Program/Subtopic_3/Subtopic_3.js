593de1c338a33698d580a10f790bbe0b69b0341c
Luong Nguyen Khoa <cucdaunho@gmai.com>
2023-09-03 21:01:44 +0700

    RWD/GUI - Fix the design of project 3

--- GUI/src/Pilot_Program/Subtopic_3/Subtopic_3.js
+++ GUI/src/Pilot_Program/Subtopic_3/Subtopic_3.js
@@ -8,43 +8,46 @@ import ProjectVideosSetUp, { SetupVideos } from "./setupVideos/setupVideos";
 const Videos = [
     {
         id: "1",
-        title: "Meeting 1",
+        title: "MEETING 1",
         className: "M",
         source: "https://drive.google.com/file/d/1sN-XPPQ44--DFJGY949Ua06R8Izbln2H/preview",
     },
     {
         id: "2",
-        title: "Meeting 2",
+        title: "MEETING 2",
         className: "M",
         source: "https://drive.google.com/file/d/1uw56krHZ0uVB3u07mX6mX0vI4E98VS1J/preview"
     },
     {
         id: "3",
-        title: "Meeting 3",
+        title: "MEETING 3",
         className: "M",
         source: "https://drive.google.com/file/d/1_o3t_xDpebyNy9uYZcPnY2BKtS0AtK1h/preview"
     },
     {
         id: "4",
-        title: "Meeting 4",
+        title: "MEETING 4",
         className: "M",
         source: "https://drive.google.com/file/d/1ElmLlOoN17pg_t-CgcVz31rOHGHUhfXY/preview"
     },
     {
         id: "5",
-        title: "Meeting 5",
+        title: "MEETING 5",
         className: "M",
         source: "https://drive.google.com/file/d/1k1Lk0W6lVZBUMB5LGge5_k0Mv-1Bs6fJ/preview"
     }
 ];
 
 function ProjectVideos() {
-  const [currentVideo, setCurrentVideo] = useState("1");
+    const [currentVideo, setCurrentVideo] = useState("1");
     return (
         <div className="Videos_body">
             <Tool />
             <Tool_mobile />
             <SDGslogo />
+            <div className="header">
+                <h2>PROJECT 3</h2>
+            </div>
             <div className="Videos">
                 {Videos.map((item) => (
                     <div key={item.id} className="video-container">
@@ -61,6 +64,7 @@ function ProjectVideos() {
                     </div>
                 ))}
             </div>
+            <div className = "meow"></div>
             <ProjectVideosSetUp currentID={currentVideo} />
             <Footer />
             <SetupVideos />
